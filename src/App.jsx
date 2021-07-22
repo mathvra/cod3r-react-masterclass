ccb3230889502005e364018c9e85698028a8c996
mathvra <matheusvram@gmail.com>
2021-07-21 21:58:00 -0300

    primeira aula do masterclas

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComIf'
+
+export default props =>
+    <div className = "App">
+        <Card titulo = "#06- Condicional v2">
+            <CondicionalComIf numero={10}></CondicionalComIf>
+        </Card>
+        <Card titulo = "#05- Condicional v1">
+            <Condicional numero={11}></Condicional>
+        </Card>
+        <Card titulo = "#04 - Componente com Repetição">
+            <Repeticao></Repeticao>
+        </Card>
+        <Card titulo = "#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
+        <Card titulo = "#02 - Componente com Parametro">
+            <ComParametro
+                titulo = "Esse é o titulo"
+                subtitulo = "Esse é o subtitulo"
+            />
+        </Card>
+        <Card titulo = "#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+    </div>
---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () =>
+    <>
+        <h1>Primeiro Componente (Arrow)</h1>
+        <h1>Exemplo de um componente React</h1>
+    </>
